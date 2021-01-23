3ee1e1e0acd0f10fffbc5a016e8ab6595eb1cb96
Oussama Heloulou <oussama.hel@gmail.com>
2021-01-23 22:36:58 +0100

    Use functions in the script

--- script.py
+++ script.py
@@ -16,45 +16,63 @@ import re
 from os import listdir
 from os.path import isfile, join
 
-suffixes = "(adj\.|adv\.|pron\.|num\.|num\.-m|conj\.|part\.|aux\.|prep\.|n\.|v\.|m\.|phrw\.)"
+suffixes = "(adj\.|adv\.|pron\.|num\.|num\.-m|conj\.|part\.|aux\.|prep\.|n\.|v\.|m\.|phrw\.|\.\.\.)"
 base_str1 = """<p style="text-align: center;"><span class="large">%s</span></p>"""
 base_str2 = """<p style="text-align: center;"><span class="large">%s </span></p><p style="text-align: center;"><span class="large">%s</span></p>"""
 
 
 def extract_suffix(word):
+    # Return a pinyin that is like wordsfx. or fword swordsfx. as: ['word', 'sfx.'] or ['fword', 'sword', 'sfx.']
     regex = f'^(\w+?\s?\w+?\t?)((?:{suffixes}) .*)$'
     return re.sub(regex, "\\1#\\2", word, re.UNICODE).split("#")
 
 
-files = []
-files_dir = join('.', 'input-files')
-for file in listdir(files_dir):
-    if isfile(join(files_dir, file)) and file.endswith('.csv'):
-        files.append((files_dir, file))
+def get_file_list(dir):
+    """
+    Return list of CSV files
+    """
+    files = list()
+    for file in listdir(dir):
+        if isfile(join(dir, file)) and file.endswith('.csv'):
+            files.append((dir, file))
+    return files
 
-result = []
-for file in files:
+
+def read_file(file):
+    result = list()
     filename = join(*file)
-    print("Opening file: ",filename)
-    # JLK added , encoding='utf-8'
+    print("Opening file: ", filename)
     with open(filename, encoding='utf-8') as input_file:
         reader = csv.reader(input_file)
         # skip header
         next(reader)
         for i, row in enumerate(reader, start=2):
             ch_letters = re.findall(r'[\u4e00-\u9fff]+', row[1])
+            # log the line that doesn't match the format and exit
             if ch_letters:
                 print(f'This line [{i}] in the file [{file[1]}] does not match the format')
                 exit()
             spell_def = extract_suffix(row[1])
-            # JLK: Print for debugging line by line
-            #print("spell_def:", spell_def)
             result.append((base_str1 % row[0], base_str2 % (spell_def[0], spell_def[1])))
+    return result
 
-    # JLK added:  , encoding='utf-8'
+
+def write_file(file, data):
     with open(f'result/html-{file[1]}', 'w', encoding='utf-8', newline='') as output:
         writer = csv.writer(output,)
         writer.writerow(['Question', 'Answer'])
-        for row in result:
+        for row in data:
             writer.writerow([row[0], row[1]])
-    result = list()
+
+
+def main():
+    files_dir = join('.', 'input-files')
+    files = get_file_list(files_dir)
+
+    for file in files:
+        data = read_file(file)
+        write_file(file, data)
+
+
+if __name__ == '__main__':
+    main()
